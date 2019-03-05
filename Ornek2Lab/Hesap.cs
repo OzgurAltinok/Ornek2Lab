7b7a9cd089c88f071a6c499c9ea59683f368cc45
ozguraltinok <48184394+ozguraltinok@users.noreply.github.com>
2019-03-05 23:41:12 +0300

    Lab02 ornegi yapildi

---
+++ Ornek2Lab/Hesap.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek2Lab
+{
+    class Hesap
+    {
+
+        public static void MerkezKoordinatDikdortgen(Dikdortgen t)
+        {
+            Console.WriteLine("Merkez coord: x: " + t.coordDikdortgen.xkoordinati * 1 / 2 + "\nMerkez coord: y: " + t.coordDikdortgen.ykoordinati * 1 / 2);
+        }
+
+        public static void MerkezKoordinatDaire(Daire d)
+        {
+            Console.WriteLine("Merkez coord: x: " + d.coordDaire.xkoordinati + d.yCap + "\nMerkez coord: y: " + d.coordDaire.ykoordinati + d.yCap);
+        }
+
+        public static double CevreHesaplaDikdortgen(Dikdortgen t)
+        {
+            return 2 * (t.genislik + t.yukseklik);
+        }
+
+        public static double CevreHesaplaDaire(Daire d)
+        {
+            return 2 * Math.PI * d.yCap;
+        }
+
+        public static double AlanHesaplaDikdortgen(Dikdortgen t)
+        {
+            return t.yukseklik * t.genislik;
+        }
+
+        public static double AlanHesaplaDaire(Daire d)
+        {
+            return Math.PI * Math.Pow(d.yCap, 2);
+        }
+    }
+
+    public struct Koordinat
+    {
+        public int xkoordinati, ykoordinati;
+    }
+
+    public struct Daire
+    {
+        public int yCap;
+        public Koordinat coordDaire;
+    }
+
+    public struct Dikdortgen
+    {
+        public int yukseklik, genislik;
+        public Koordinat coordDikdortgen;
+
+    }
+}
---
+++ Ornek2Lab/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ornek2Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Daire deneme = new Daire();
+            deneme.coordDaire.xkoordinati = 3;
+            deneme.coordDaire.ykoordinati = 4;
+            deneme.yCap = 3;
+
+            Dikdortgen deneme2 = new Dikdortgen();
+            deneme2.coordDikdortgen.xkoordinati = 3;
+            deneme2.coordDikdortgen.ykoordinati = 4;
+            deneme2.genislik = 3;
+            deneme2.yukseklik = 5;
+
+            Console.WriteLine("Daire ornegi" + " yaricap= " + deneme.yCap + " koordinatlar: " + deneme.coordDaire.xkoordinati + " , " + deneme.coordDaire.ykoordinati);
+            Console.WriteLine("Daire alan = " + Hesap.AlanHesaplaDaire(deneme));
+            Console.WriteLine("Daire cevre = " + Hesap.CevreHesaplaDaire(deneme));
+            Hesap.MerkezKoordinatDaire(deneme);
+
+            Console.WriteLine("\nDikdortgen ornegi");
+            Console.WriteLine("Daire ornegi" + " genislik = " + deneme2.genislik + " yukseklik =  " + deneme2.yukseklik + " koordinatlar: " + deneme2.coordDikdortgen.xkoordinati + " , " + deneme2.coordDikdortgen.xkoordinati);
+            Console.WriteLine("Dikdortgen alan = " + Hesap.AlanHesaplaDikdortgen(deneme2));
+            Console.WriteLine("Dikdortgen cevre = " + Hesap.CevreHesaplaDikdortgen(deneme2));
+            Hesap.MerkezKoordinatDikdortgen(deneme2);
+
+            Console.ReadKey();
+        }
+    }
+}
